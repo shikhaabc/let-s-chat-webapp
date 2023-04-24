bc4f7d2c35779266a84c6f8ec721116c25baf51c
shikhaabc <106334405+shikhaabc@users.noreply.github.com>
2023-04-24 17:31:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function Start(){
+    window.location="index.html";
+}
+
+function adduser() {
+    user_name = document.getElementById("user_name").value;
+
+    localStorage.setItem("Username",user_name);
+
+    window.location = "kwitter_room.html";
+}
